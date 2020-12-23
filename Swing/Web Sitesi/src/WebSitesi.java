5ddb5df714a4856cadc2ede1da2477507dce2446
Ahmet Candan <ahmetcandan1@outlook.com.tr>
2020-12-23 23:49:04 +0300

    Swing

--- Swing/Web Sitesi/src/WebSitesi.java
+++ Swing/Web Sitesi/src/WebSitesi.java
@@ -1,6 +1,39 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class WebSitesi {
-    private JPanel main;
-    private JButton button1;
+public class WebSitesi extends JFrame{
+    private JPanel panel1;
+    private JButton denemeButton;
+    private JTextField textField1;
+
+
+    public WebSitesi(){
+
+        add(panel1);
+        setSize(500,300);
+        setTitle("Ödev bitti h.o.");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+        denemeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String adres = "https://www.obilet.com";
+                Desktop d = Desktop.getDesktop();
+                try {
+                    d.browse(new URI(adres));
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } catch (URISyntaxException uriSyntaxException) {
+                    uriSyntaxException.printStackTrace();
+                }
+
+            }
+        });
+    }
 }
